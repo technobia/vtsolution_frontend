d894b92809201cbfd96e045b86e4f18bf3e0d3a7
apium <technobia@gmail.com>
2015-07-25 10:00:08 +0700

    update server

--- grunt/config/http-server.js
+++ grunt/config/http-server.js
@@ -3,14 +3,14 @@
  */
 module.exports = function(grunt) {
     return {
-        dev: {
-            root: '.',
-            host: '0.0.0.0',
-            port: '9000',
-            cache: 0, // no cache!
-            showDir: false,
-            autoIndex: false,
-            runInBackground: false
+        'dev': {
+            'root': '.',
+            'host': '0.0.0.0',
+            'port': '9000',
+            'cache': 0, // no cache!
+            'showDir': false,
+            'autoIndex': false,
+            'runInBackground': false
         }
     };
 };
